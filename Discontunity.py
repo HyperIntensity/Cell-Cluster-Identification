4d346c0759b46085a545c13ccc773670414fddc9
HyperIntensity <92801964+HyperIntensity@users.noreply.github.com>
2023-06-29 10:22:53 -0500

    Add files via upload

---
+++ Discontunity.py
@@ -0,0 +1,96 @@
+''''
+This program is able to detect discontinuity amongst cell clusters. 
+Author: 
+Date: 6/28/2023
+Note: Still a working prototype  
+Inspired by: fmw42(Stackoverflow) and Samhitha Kolla(samhitha.kolla@wustl.edu)
+'''''
+
+#Import Statments
+import cv2
+import numpy as np
+import pandas as pd
+from scipy import ndimage
+from skimage.feature import peak_local_max
+from skimage.segmentation import watershed
+import math
+import warnings
+warnings.filterwarnings("ignore")
+
+#Reading path file and performing necessary operations
+pathfile = "C:\Users\yssin\PycharmProjects\pythonProject\Captured CY5 4.tif"
+img = cv2.imread(pathfile)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(gray, 127, 255, 1)
+
+#Creating a structuring element to perform morphological operations
+kernel = np.ones((7, 7),np.uint8)
+erosion = cv2.morphologyEx(thresh, cv2.MORPH_ERODE, kernel, iterations = 2)
+
+ret, thresh1 = cv2.threshold(erosion, 127, 255, 1)
+blur = cv2.blur(thresh1, (15, 15))
+
+ret, thresh2 = cv2.threshold(blur, 145, 255, 0)
+
+#Performing a second morphological operation.
+kernel1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+final = cv2.morphologyEx(thresh2, cv2.MORPH_ERODE, kernel1, iterations = 2)
+
+#Finding Contours within the image
+contours,hierarchy = cv2.findContours(final, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+#Credit: Samhitha Kolla
+def count_points(roi):
+   """
+   This function counts the number of points(secretions) within the clusters
+   :param roi: Region of Interest
+   :return: labels, unique label counts
+   """
+   # Performing Mean Shift Filtering
+   shifted = cv2.pyrMeanShiftFiltering(roi, 21, 51)
+
+   # Converting the image to grayscale
+   gray = cv2.cvtColor(shifted, cv2.COLOR_BGR2GRAY)
+
+   # Thresholding using Binary and OTSU
+   thrsh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+   # Using Watershed Algorithm
+   D = ndimage.distance_transform_edt(thrsh)
+   localMax = peak_local_max(D, indices=False, min_distance=1, labels=thrsh)
+   markers = ndimage.label(localMax)[0]
+   lbls = watershed(-D, markers, mask=thrsh)
+
+   return lbls, len(np.unique(lbls)) - 1
+
+# Find the convex hull for all the contours
+ClusterCounter = 0
+for cnt in contours:
+   x, y, w, h = cv2.boundingRect(cnt)
+   size = 80 #Change the size according to the images(Refer to README)
+
+   # Filtering out smaller clusters.
+   if size < w < final.shape[0] and size < h < final.shape[1]:
+      hull = cv2.convexHull(cnt)
+
+      # Comment for a Silhouette Contour
+      #final = cv2.drawContours(img,[cnt],0,(0,255,0),10)
+
+      # Comment for a polygon Countour
+      final = cv2.drawContours(img,[hull],0,(0,0,255),10)
+      ClusterCounter += 1
+      ROI = final[y:y + h, x:x + w]
+
+      # Counting the number of secretions within each cluster
+      lbls, count = count_points(ROI)
+
+
+# Display the image with contours drawn
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+filename = pathfile.split(".")[0]
+cv2.imwrite(pathfile + '_Output.jpg', final)
+with open(filename + "_Output.txt", "w") as file:
+   file.write("Image Captured: " + str(filename) + "\n")
+   cv2.imwrite(pathfile + '_Output.jpg', final)
+   file.write("Clusters: " + str(ClusterCounter) + "\n")
+
